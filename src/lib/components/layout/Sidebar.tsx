521f5da51729bc4bffc382908254d58d9dc775f5
setbaq <ebr.sina@outlook.com>
2022-04-24 04:08:25 +0430

    remove transalte animation

--- src/lib/components/layout/Sidebar.tsx
+++ src/lib/components/layout/Sidebar.tsx
@@ -63,9 +63,9 @@ export default function SidebarWithHeader({
     }
   }, []);
   const variants = {
-    hidden: { opacity: 0, x: -50, y: 0 },
-    enter: { opacity: 1, x: 0, y: 0 },
-    exit: { opacity: 0, x: 0, y: -50 },
+    hidden: { opacity: 0.7, },
+    enter: { opacity: 1 },
+    exit: { opacity: 0.7, },
   };
   return (
     <MotionBox
